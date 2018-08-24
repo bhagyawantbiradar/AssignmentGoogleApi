85c1c5f9d8200a2faebcf1e7e12aaf870adcab14
Bhagyawant <bhagyawantbiradar@gmail.com>
2018-08-24 21:53:15 +0530

    initial

---
+++ app/src/main/java/com/example/solo/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.solo.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
